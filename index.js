b05d7a68086675ca6e25b4b24600bed9de382abf
Hector B <hectorbg303@gmail.com>
2025-05-10 14:13:26 +0200

    Wheel and key events

--- index.js
+++ index.js
@@ -3,32 +3,67 @@ const slides = document.querySelectorAll(".slide");
 
 let index = 0;
 
-function handleClickSlider(ev) {
-  if (ev.target.closest(".btn--prev")) {
-    if (index > 0) {
-      index--;
-    } else {
-      index = slides.length - 1;
-    }
-  } else if (ev.target.closest(".btn--next")) {
-    if (index < slides.length - 1) {
-      index++;
-    } else {
-      index = 0;
-    }
-  }
-  goToSlide(index);
-}
-
 function goToSlide(n) {
   slides.forEach((slide, i) => {
     slide.style.transform = `translateX(${100 * (i - n)}%)`;
   });
 }
 
+function increaseIndex() {
+  if (index < slides.length - 1) {
+    index++;
+  } else {
+    index = 0;
+  }
+}
+
+function decreaseIndex() {
+  if (index > 0) {
+    index--;
+  } else {
+    index = slides.length - 1;
+  }
+}
+
+// Handlers
+
+function handleClickSlider(ev) {
+  if (ev.target.closest(".btn--prev")) {
+    decreaseIndex();
+  } else if (ev.target.closest(".btn--next")) {
+    increaseIndex();
+  }
+  goToSlide(index);
+}
+
+function handleWheelSlider(ev) {
+  if (ev.deltaY < 0) {
+    decreaseIndex();
+  } else if (ev.deltaY > 0) {
+    increaseIndex();
+  }
+  goToSlide(index);
+}
+
+function handleKeypress(ev) {
+  const key = ev.key;
+  if (key === "ArrowLeft") {
+    decreaseIndex();
+  } else if (key === "ArrowRight") {
+    increaseIndex();
+  } else {
+    return;
+  }
+  goToSlide(index);
+}
+
+//////////////////////
+
 function init() {
   goToSlide(index);
   slider.addEventListener("click", handleClickSlider);
+  slider.addEventListener("wheel", handleWheelSlider);
+  document.addEventListener("keydown", handleKeypress);
 
   setTimeout(function () {
     slider.style.opacity = 1;
